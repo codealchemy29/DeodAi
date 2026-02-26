2247c1e170512830ad3aaf9a6099e0212ed9b95c
codealchemy29 <mandalpayal.2906@gmail.com>
2026-02-26 12:41:44 +0530

    Initial commit

--- client/src/config/env.ts
+++
@@ -1,3 +0,0 @@
-export const CLAIM_URL = import.meta.env.VITE_BIZAISKILL;
-export const TOKEN_CONTRACT_ADDRESS = import.meta.env.DEOD_TOKEN_CONTRACT;
-export const RECIPIENT_ADDRESS = import.meta.env.VITE_RECIPIENT_ADDRESS;
